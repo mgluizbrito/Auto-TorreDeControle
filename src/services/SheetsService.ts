d8c71089e445df8995246831a1a50fc891702765
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-09-25 22:54:22 -0300

    feat: add logging and getDriverPhone from sheet function

--- src/services/SheetsService.ts
+++ src/services/SheetsService.ts
@@ -1,28 +1,78 @@
 import { getSheetData } from '../controllers/SheetsController.js';
+import logger from '../utils/logger.js';
 
 export async function getDailyPendingTransfers(formattedDate: string): Promise<string[][]> {
-    const range = `MIDDLE MILE!A:AA`;
+    logger.info(`Buscando viagens PENDENTES para a data: ${formattedDate}`);
+    const range: string = `MIDDLE MILE!A:AA`;
 
-    const allData = await getSheetData(range);
+    try {
+        const allData: any[] | null = await getSheetData(range);
 
-    if (!allData || allData.length === 0) throw new Error('Nenhum dado encontrado na planilha.');
+        if (!allData || allData.length === 0){
+            logger.warn('Nenhuma viagem encontrada na planilha.');
+            return [];
+        }
 
-    const filteredRows = allData.filter(row => {
-        const date = row[0]; // Coluna A (Data) é o índice 0
-        const status = row[26]; // Coluna AA (Status) é o índice 26
+        const filteredRows = allData.filter(row => {
+            const date = row[0]; // Coluna A (Data) é o índice 0
+            const status = row[26]; // Coluna AA (Status) é o índice 26
 
-        return row && date === formattedDate && status === 'PENDENTE';
-    });
+            return row && date === formattedDate && status === 'PENDENTE';
+        });
 
-    const finalData = filteredRows.map(row => {
+        const finalData = filteredRows.map(row => {
 
-        const date = row[0]; // Coluna A: índice 0
-        const driverName = row[13]; // Coluna N: índice 13
-        const presentationTime = row[18]; // Coluna S: índice 18
-        const status = row[26]; // Coluna AA: índice 26
+            const date = row[0]; // Coluna A: índice 0
+            const driverName: string = row[13]; // Coluna N: índice 13
+            const presentationTime: string = row[18]; // Coluna S: índice 18
+            const status = row[26]; // Coluna AA: índice 26
 
-        return [date, driverName, presentationTime, status];
-    });
+            return [date, driverName, presentationTime, status];
+        });
+        
+        logger.info(`Encontradas ${finalData.length} viagens PENDENTES`);
+        logger.debug(`Detalhes das viagens PENDENTES: ${JSON.stringify(finalData)}`);
+        return finalData;
     
-    return finalData;
+    } catch (error) {
+        logger.error(`Erro ao buscar dados da planilha: ${error instanceof Error ? error.message : error}`);
+        throw error;
+    }
+}
+
+export async function getDriverData(driverName: string): Promise<string[][]> {
+    logger.info(`Buscando dados do motorista: ${driverName}`);
+    const range: string = `Motoristas!D:H`;
+
+    try {
+        const allData: any[] | null = await getSheetData(range);
+
+        if (!allData || allData.length === 0){
+            logger.warn('Nenhum dado de motorista encontrado na planilha.');
+            return [];
+        }
+
+        const findedDriver = allData.filter(row => {
+            return row && row[0] === driverName;
+        });
+
+        if (findedDriver.length === 0) {
+            logger.warn(`Motorista ${driverName} não encontrado na planilha.`);
+            return [];
+        }
+
+        const finalData = findedDriver.map(row => {
+
+            const name: string = row[0]; // Coluna D: índice 0
+            const phone: string = row[4].replace("+", ""); // Coluna H: índice 4
+            
+            return [name, phone];
+        });
+
+        return finalData;
+
+    } catch (error) {
+        logger.error(`Erro ao buscar dados da planilha: ${error instanceof Error ? error.message : error}`);
+        throw error;
+    }
 }
