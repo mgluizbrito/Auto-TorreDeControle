cc6d8afdd8a66829fe64f85b9ae5d151cf57e6e9
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-09-25 22:56:00 -0300

    feat: wwebjs sendMessage function

---
+++ src/controllers/WWebController.ts
@@ -0,0 +1,36 @@
+import 'dotenv/config';
+import { wppClient } from '../auth/WWebAuth.js';
+import logger from '../utils/logger.js';
+
+function getWhatsAppId(phoneNumber: string): string {
+    // Adiciona o sufixo @c.us para IDs de contato (chat user)
+    return `${phoneNumber}@c.us`; 
+}
+
+export async function sendWppMessage(phone: string, msg: string): Promise<void> {
+    
+    if (!wppClient.info) {
+        logger.error("Cliente WhatsApp não está pronto ou não foi inicializado.");
+        return;
+    }
+    
+    const chatId = getWhatsAppId(phone);
+
+    try {
+        // Verifica se o ID é válido e se existe no WhatsApp
+        if (!await wppClient.isRegisteredUser(chatId)) {
+            logger.warn(`Número não registrado no WhatsApp: ${phone}`);
+            return;
+        }
+
+        // Envia a mensagem
+        await wppClient.sendMessage(chatId, msg);
+        logger.info(`Mensagem enviada com sucesso para ${phone}`);
+
+    } catch (error) {
+        logger.error(`Falha ao enviar mensagem para ${phone}: ${error}`);
+        throw error;
+    }
+
+    return;
+}
