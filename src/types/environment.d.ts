5f1f7cec1b5f1f2bdf58c082856575b9978a8ead
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-09-25 22:52:30 -0300

    fix: minor changes

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -2,6 +2,9 @@ declare namespace NodeJS {
     interface ProcessEnv {
 
         SPREADSHEET_ID: string;
+        EXECUTE_CYCLE_MS: number;
+        NODE_ENV: 'development' | 'production';
+        TIME_WINDOW_MINUTES: string;
         CLIENT_ID: string;
         CLIENT_SECRET: string;
     }
