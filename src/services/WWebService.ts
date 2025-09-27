c932636e980033765f16bb8d2a583ad6f87ead6e
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-09-27 11:56:17 -0300

    feat: sendAlertMessage function on WWebService

---
+++ src/services/WWebService.ts
@@ -0,0 +1,29 @@
+import pkg from 'whatsapp-web.js';
+import { sendWppMessage } from '../controllers/WWebController.js';
+import logger from '../utils/logger.js';
+import {getDriverData} from './SheetsService.js'
+
+let wppClient: pkg.Client;
+
+export async function sendAlertMessageToDriver(driverName: string | undefined, alertMessage: string): Promise<void>{
+
+    if (!driverName) return;
+    const driverDataArray: string[][] = await getDriverData(driverName);
+
+    const driverData: string[] | undefined = driverDataArray[0];
+    if (!driverData) return;
+
+    if (driverData[1]) {
+        
+        sendWppMessage(wppClient, driverData[1], alertMessage);
+
+    } else {
+        logger.warn(`Número de telefone não encontrado para o motorista: ${driverName}`);
+        return;
+    }
+}
+
+export function setWppClient(client: pkg.Client): void {
+    wppClient = client;
+    logger.info('Cliente WhatsApp definido no serviço WWebService.');
+}
