54471e5f30f9c59eefa22f89526dffa201fff43c
Luiz Miguel Xavier de Brito <mgluiz1781@gmail.com>
2025-09-29 23:03:36 -0300

    style: converted to class

--- src/controllers/WWebController.ts
+++ src/controllers/WWebController.ts
@@ -1,29 +1,36 @@
 import pkg from 'whatsapp-web.js';
-import { sendWppMessage } from '../services/WWebService.js';
+import WWebService from '../services/WWebService.js';
 import logger from '../utils/logger.js';
 import {getDriverData} from './SheetsController.js'
 
-let wppClient: pkg.Client;
+let wppService: WWebService;
 
-export async function sendAlertMessageToDriver(driverName: string | undefined, alertMessage: string): Promise<void>{
+class WWebController{
 
-    if (!driverName) return;
-    const driverDataArray: string[][] = await getDriverData(driverName);
-
-    const driverData: string[] | undefined = driverDataArray[0];
-    if (!driverData) return;
-
-    if (driverData[1]) {
-        
-        sendWppMessage(wppClient, driverData[1], alertMessage);
-
-    } else {
-        logger.warn(`Número de telefone não encontrado para o motorista: ${driverName}`);
-        return;
+    constructor(client: pkg.Client) {
+        wppService = new WWebService(client);
+        logger.info('Cliente WhatsApp definido no serviço WWebService.');
     }
+
+    async sendAlertMessageToDriver(driverName: string | undefined, alertMessage: string): Promise<void>{
+
+        if (!driverName) return;
+        const driverDataArray: string[][] = await getDriverData(driverName);
+
+        const driverData: string[] | undefined = driverDataArray[0];
+        if (!driverData) return;
+
+        if (driverData[1]) {
+            
+            wppService.sendWppMessage(driverData[1], alertMessage);
+
+        } else {
+            logger.warn(`Número de telefone não encontrado para o motorista: ${driverName}`);
+            return;
+        }
+    }
+
+    
 }
 
-export function setWppClient(client: pkg.Client): void {
-    wppClient = client;
-    logger.info('Cliente WhatsApp definido no serviço WWebService.');
-}
+export default WWebController;
